dd6a901452ebc8fe80b51a03c483757f3cb6a6c2
amilasilva7 <osapsilva@gmail.com>
2025-05-31 08:39:29 +0530

    First commit

---
+++ src/main/java/org/ostech/tng/TngApplication.java
@@ -0,0 +1,13 @@
+package org.ostech.tng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ostech/tng/TngApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostech.tng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
